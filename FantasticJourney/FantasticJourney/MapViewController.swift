c393faa2866245cddd6908dc0b760f09edf07f9f
Ariel Rodriguez <ariel@volonbolon.me>
2017-09-14 16:21:23 -0300

    Load location from core data store

--- FantasticJourney/FantasticJourney/MapViewController.swift
+++ FantasticJourney/FantasticJourney/MapViewController.swift
@@ -16,26 +16,36 @@ class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    var _context:NSManagedObjectContext?
+    var context:NSManagedObjectContext {
+        get {
+            if self._context == nil {
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                let context = appDelegate.persistentContainer.viewContext
+                self._context = context
+            }
+            return self._context!
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let locationFetch = NSFetchRequest<NSFetchRequestResult>(entityName: Location.ManagedObjectName)
+        
+        do {
+            let fetch = try self.context.fetch(locationFetch) as! [Location]
+            for l in fetch {
+                print(l)
+            }
+        } catch {
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension MapViewController:MKMapViewDelegate {
@@ -44,6 +54,31 @@ extension MapViewController:MKMapViewDelegate {
             return "mapAnnotation"
         }
     }
+    
+//    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+//
+//        let mapRegion = mapView.region
+//        var annotations:[MKPointAnnotation] = []
+//        for _ in 0..<215 {
+//            let c = mapRegion.randomCoordinate()
+//            let location = NSEntityDescription.insertNewObject(forEntityName: Location.ManagedObjectName, into: context) as! Location
+//            location.latitude = c.latitude
+//            location.longitude = c.longitude
+//            location.timestamp = Date() as NSDate
+//            location.origin = Int16(arc4random_uniform(2)) + 1
+//            
+//            let annotation = MKPointAnnotation()
+//            annotation.coordinate = c
+//            annotations.append(annotation)
+//        }
+//        mapView.addAnnotations(annotations)
+//        
+//        do {
+//            try context.save()
+//        } catch {
+//            print(error)
+//        }
+//    }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         let reuseIdentifier = MapViewController.reuseIdentifer
