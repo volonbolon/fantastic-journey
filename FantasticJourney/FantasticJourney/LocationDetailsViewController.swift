db6cd09a2128026667a77ea8ebfea9b7977cf835
Ariel Rodriguez <ariel@volonbolon.me>
2017-09-19 12:04:51 -0300

    Present location details

--- FantasticJourney/FantasticJourney/LocationDetailsViewController.swift
+++ FantasticJourney/FantasticJourney/LocationDetailsViewController.swift
@@ -13,15 +13,33 @@ class LocationDetailsViewController: UIViewController {
     var location:Location!
     @IBOutlet weak var mapView:MKMapView!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.title = "\(self.location.locationOrigin) || "
-
-        // Do any additional setup after loading the view.
+        self.title = location.title
+        
+        let av = LocationAnnotation(location:self.location)
+        self.mapView.addAnnotation(av)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: self.location.coordinate, span: span)
+        self.mapView.setRegion(region, animated: false)
+        
+        self.segmentedControl.selectedSegmentIndex = Int(location.rating)
     }
 
+    @IBAction func ratingChanged(_ sender: Any) {
+        let lr = LocationRating(rawValue: Int16(self.segmentedControl.selectedSegmentIndex))!
+        self.location.locationRating = lr
+        
+        do {
+            try self.location.managedObjectContext?.save()
+        } catch {
+            print(error)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
