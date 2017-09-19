572ef3e5e953319441e93d0bd6fe47ab083c7b8b
Ariel Rodriguez <ariel@volonbolon.me>
2017-09-19 10:56:37 -0300

    Add Constants

---
+++ FantasticJourney/FantasticJourney/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  FantasticJourney
+//
+//  Created by Ariel Rodriguez on 9/19/17.
+//  Copyright © 2017 Sp0n. All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    struct StoryboardsIdentifier {
+        static let ShowDetailsFromMap = "showDetailsFromMap"
+        static let ShowDetailsFromList = "showDetailsFromList"
+    }
+}
